0faf8a64f5d57a347dd852fc666be31c817246a6
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-03-27 22:32:41 +0800

    adding seeded data tp db

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
            var host = CreateHostBuilder(args).Build();
 
@@ -27,7 +27,8 @@ namespace API
            try
            {
               var context = services.GetRequiredService<DataContext>();
-              context.Database.Migrate();
+              await context.Database.MigrateAsync();
+              await Seed.SeedData(context);
            } 
            catch (Exception ex)
            {
@@ -35,7 +36,7 @@ namespace API
               logger.LogError(ex, "An error occured during migration");
            }
 
-           host.Run();
+           await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
