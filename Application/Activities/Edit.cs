3c303f99fbcf0ad720c364aaedaeaaf70e6e3ef1
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-03-29 09:22:23 +0800

    adding Automapper

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 //we bring in domain to be able to use the activity from details
 using Domain;
 using MediatR;
+using AutoMapper;
 using Persistence;
 using Application.Activities;
 
@@ -19,8 +20,11 @@ namespace Application.Activites
     public class Handler : IRequestHandler<Command>
     {
         private readonly DataContext _context;
-        public Handler(DataContext context)
+
+        private readonly IMapper _mapper;
+        public Handler(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
         }
 
@@ -28,7 +32,11 @@ namespace Application.Activites
         {
             var activity = await _context.Activities.FindAsync(request.Activity.Id);
             
-            activity.Title = request.Activity.Title ?? activity.Title;
+            //this sets the props manually
+          //  activity.Title = request.Activity.Title ?? activity.Title;
+
+             //****** AUTOMAPPER
+             _mapper.Map(request.Activity, activity);
 
             await _context.SaveChangesAsync();
 
