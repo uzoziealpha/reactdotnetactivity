a0c272daddf1e522954c8d84054bbbf223404a64
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-03-30 11:50:33 +0800

    adding canceling button and forms for create activity

--- client-app/src/features/activities/ActivityList.tsx
+++ client-app/src/features/activities/ActivityList.tsx
@@ -7,10 +7,12 @@ import { Button,  Item, Label, Segment } from 'semantic-ui-react';
 
 interface Props {
     activities: Activity[];
+    selectActivity: (id: string) => void;
+
 }
 
 
-export default function ActivityList({activities}: Props) {
+export default function ActivityList({activities, selectActivity}: Props) {
     return (
        <Segment>
          <Item.Group divided>
@@ -30,7 +32,7 @@ export default function ActivityList({activities}: Props) {
                      </div>
                  </Item.Description>
                  <Item.Extra>
-                     <Button floated='right' content='view' colors='blue' /> 
+                     <Button onClick={() => selectActivity(activity.id)} floated='right' content='view' colors='blue' /> 
                      <Label basic content={activity.category}/>
                  </Item.Extra>
                   </Item.Content>
