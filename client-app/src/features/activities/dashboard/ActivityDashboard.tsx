f90e4620a7a73ca9fe0aa1d0c7e645f25d565a4f
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-03-30 09:08:28 +0800

    adding the details forms with semantic-ui

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Grid , List} from "semantic-ui-react";
 import { Activity } from '../../../app/models/activity';
+import ActivityList from "../ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 
 interface Props {
@@ -13,13 +16,12 @@ export default function ActivityDashboard({activities}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-            <List>
-              {activities.map(activity => (
-                <List.Item key={activity.id}>
-                   {activity.title}
-                </List.Item>
-          ))}
-         </List>
+              <ActivityList activities={activities}/>
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] && 
+                <ActivityDetails activity={activities[0]} />}
+                <ActivityForm/>
             </Grid.Column>
         </Grid>
     )
